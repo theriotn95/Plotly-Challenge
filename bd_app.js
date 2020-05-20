9d88b04e86a35f8c86d9d56e9d673c1470147dcc
theriotn95 <theriotn95@gmail.com>
2020-05-20 16:19:02 -0700

    saving progress

--- bd_app.js
+++ bd_app.js
@@ -1,13 +1,25 @@
-function getPlot(id) {
+function getChart(id) {
     d3.json("samples.json").then((data)=> {
-   
+        var freq = data.metadata.map(d => d.freq)
+       
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
         
-   
-   
-   
-   
-   
-   
-   
-   
-    };
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
+  
+        var otu_top = (samples.otu_ids.slice(0, 10)).reverse();
+        
+        var otu_id = otu_top.map(d => "OTU " + d)
+  
+        var labels = samples.otu_labels.slice(0, 10);
+  
+        
+        var trace1 = {
+            x: samplevalues,
+            y: otu_id,
+            text: labels,
+            type:"bar",
+            orientation: "h",
+        };
+        
+
+
